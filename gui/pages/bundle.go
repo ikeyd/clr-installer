23ded9abfdb840081d0663d0333069e82509b0bf
Ikey Doherty <ikey.doherty@intel.com>
2019-01-17 14:24:51 +0000

    pages: Add actual content in Bundle page
    Signed-off-by: Ikey Doherty <ikey.doherty@intel.com>

--- gui/pages/bundle.go
+++ gui/pages/bundle.go
@@ -5,35 +5,80 @@
 package pages
 
 import (
+	"fmt"
+	"github.com/clearlinux/clr-installer/swupd"
 	"github.com/gotk3/gotk3/gtk"
 )
 
 // Bundle is a simple page to help with Bundle settings
 type Bundle struct {
+	bundles []*swupd.Bundle     // Known bundles
+	box     *gtk.Box            // Main layout
+	checks  *gtk.Box            // Where to store checks
+	scroll  *gtk.ScrolledWindow // Scroll the checks
 }
 
 // NewBundlePage returns a new BundlePage
 func NewBundlePage() (Page, error) {
-	return &Bundle{}, nil
+	var err error
+	bundle := &Bundle{}
+
+	// Load our bundles
+	bundle.bundles, err = swupd.LoadBundleList()
+	if err != nil {
+		return nil, err
+	}
+
+	// main layout
+	bundle.box, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	bundle.box.SetBorderWidth(8)
+
+	// check list
+	bundle.checks, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	if err != nil {
+		return nil, err
+	}
+	bundle.scroll, err = gtk.ScrolledWindowNew(nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	// no horizontal scrolling
+	bundle.scroll.SetPolicy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+	bundle.scroll.Add(bundle.checks)
+	bundle.box.PackStart(bundle.scroll, true, true, 0)
+
+	for _, b := range bundle.bundles {
+		lab := fmt.Sprintf("%s - %s", b.Name, b.Desc)
+		check, err := gtk.CheckButtonNewWithLabel(lab)
+		if err != nil {
+			return nil, err
+		}
+		bundle.checks.PackStart(check, false, false, 0)
+	}
+
+	return bundle, nil
 }
 
 // IsRequired will return false as we have default values
-func (t *Bundle) IsRequired() bool {
+func (bundle *Bundle) IsRequired() bool {
 	return false
 }
 
-func (t *Bundle) GetID() int {
+func (bundle *Bundle) GetID() int {
 	return PageIDBundle
 }
 
-func (t *Bundle) GetIcon() string {
+func (bundle *Bundle) GetIcon() string {
 	return "preferences-desktop-applications"
 }
 
-func (t *Bundle) GetRootWidget() gtk.IWidget {
-	return nil
+func (bundle *Bundle) GetRootWidget() gtk.IWidget {
+	return bundle.box
 }
 
-func (t *Bundle) GetTitle() string {
+func (bundle *Bundle) GetTitle() string {
 	return "Bundle selection"
 }
